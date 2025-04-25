99caad5736a18691937903711d3c32d6400996c8
Dorine <anyangodorine474@gmail.com>
2025-04-25 12:57:31 +0300

    feat(forms): Add ClientForm to handle client registration

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import HealthProgram
+from .models import Client
 
 class HealthProgramForm(forms.ModelForm):
     class Meta:
         model = HealthProgram
         fields = ['name', 'description']
+
+class ClientForm(forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = ['name', 'age', 'gender', 'contact']        
