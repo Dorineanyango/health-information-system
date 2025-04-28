d7f7352abbf6950aa5a2452629070c6aa3b54c8c
Dorine <anyangodorine474@gmail.com>
2025-04-28 17:26:49 +0300

    removed hardcoded secret key

--- healthsystem/settings.py
+++ healthsystem/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0yhz0ore+a=gh2ef0f#s#he#o6&wny#-1%m7v17h0l)jh&lw)i'
-
+SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
